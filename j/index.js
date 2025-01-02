0dc5bacd5fbf77ade321ce10188ed2a50fb0d9c7
Pratik Pal <palpratik420@gmail.com>
2025-01-02 22:03:25 +0530

    updates

--- j/index.js
+++ j/index.js
@@ -1,57 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const links = document.querySelectorAll(".navbar .nav-link");
-  const navbarToggler = document.getElementById("navbar-toggler");
-  const navbarCollapse = document.querySelector(".navbar-collapse"); // Corrected to target the collapsible menu
-
-  // Function to update active link
-  const updateActiveLink = () => {
-    links.forEach((link) => {
-      if (link.href === window.location.href) {
-        link.classList.add("active");
-      } else {
-        link.classList.remove("active");
-      }
-    });
-  };
-
-  // Initial check to set active link
-  updateActiveLink();
-
-  // Update active link on click
-  links.forEach((link) => {
-    link.addEventListener("click", function () {
-      setTimeout(updateActiveLink, 10); // Timeout to handle hash navigation
-    });
-  });
-
-  // Update active link on hash change (for in-page navigation)
-  window.addEventListener("hashchange", updateActiveLink);
-
-  // Hamburger toggle functionality
-  if (navbarToggler) {
-    navbarToggler.addEventListener("click", function () {
-      // Toggle the 'open' class to show/hide the navbar menu
-      navbarCollapse.classList.toggle("open");
-    });
-  }
-
-  // Close the navbar when a nav link is clicked (for mobile view)
-  links.forEach((link) => {
-    link.addEventListener("click", () => {
-      if (navbarCollapse.classList.contains("open")) {
-        navbarCollapse.classList.remove("open");
-      }
-    });
+$(document).ready(function () {
+  // Accordion functionality
+  $(".accordion").on("click", function () {
+    $(this).toggleClass("active");
+    var panel = $(this).next();
+    if (panel.css("max-height") !== "0px") {
+      panel.css("max-height", "0px");
+    } else {
+      panel.css("max-height", panel.prop("scrollHeight") + "px");
+    }
   });
 
   // Scroll progress bar functionality
   const scrollProgress = document.getElementById("scroll-progress");
 
   if (scrollProgress) {
-    document.addEventListener("scroll", function () {
-      const scrollHeight =
-        document.documentElement.scrollHeight - window.innerHeight;
-      const scrollPosition = window.scrollY;
+    $(document).on("scroll", function () {
+      const scrollHeight = $(document).height() - $(window).height();
+      const scrollPosition = $(window).scrollTop();
       const progress = (scrollPosition / scrollHeight) * 100;
       scrollProgress.style.width = progress + "%";
     });
